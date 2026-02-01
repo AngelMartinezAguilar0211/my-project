c1154e49bcec8d2f371326e1eef0f3e387b90578
Angel <angel.martinez@bunsan.io>
2026-02-01 13:26:42 -0600

    Structure bug solved in function1 file

--- function1.js
+++ function1.js
@@ -1,14 +1,13 @@
-<!DOCTYPE html>
 <html>
-<head>
-    <title>Mi Primera Página JS</title>
-</2>
-<body>
-    <h1>¡Hola desde HTML!</h1>
-    <script>
-        // Este es un comentario en JavaScript
-        alert("¡Hola, mundo desde JavaScript!"); // Muestra una alerta
-        console.log("¡Hola, mundo en la consola!"); // Imprime en la consola
-    </script>
-</body>
-</html>
+    <head>
+        <title>Mi Primera Página JS</title>
+    </head>
+    <body>
+        <h1>¡Hola desde HTML!</h1>
+        <script>
+            // Este es un comentario en JavaScript alert("¡Hola, mundo desde
+            JavaScript!"); // Muestra una alerta console.log("¡Hola, mundo en la
+            consola!"); // Imprime en la consola
+        </script>
+    </body>
+</html>;
