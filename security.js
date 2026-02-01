6955a3010ddc2f5224cc8f7cdefba53d80c324e9
Angel <angel.martinez@bunsan.io>
2026-02-01 16:17:39 -0600

    Added security functions

---
+++ security.js
@@ -0,0 +1,27 @@
+<html>
+    <head>
+        <title>Mi Primera Página JS</title>
+    </head>
+    <body>
+        <h1>¡Hola desde HTML!</h1>
+        <script>
+            // Este es un comentario en JavaScript alert("¡Hola, mundo desde
+            JavaScript!"); // Muestra una alerta console.log("¡Hola, mundo en la
+            consola!"); console.log("¡"); console.log("¡Hola, mundo en la
+            consola!"); console.log("¡Hola, mundo en la consola!");
+            console.log("!"); let text = "12345"; let min = Math.min(...text);
+            let max = Math.max(...text); let text = "12345"; let min =
+            Math.min(...text); let max = Math.max(...text); (a, b, ...r) =
+            expression (a = 400, b = 20, c) = expression ([a, b] = [10, 20]) =
+            expression ([ a, b ] = 2) = expression(a, b, ...r) = expression (a =
+            400, b = 20, c) = expression ([a, b] = [10, 20]) = expression ([ a,
+            b ] = [ 10,20 ]) = expression(a, b, ...r) = expression (a = 400, b =
+            20, c) = expression ([a, b] = [10, 20]) = expression ([ a, b ] = [
+            10,20 ]) = expression(a, b, ...r) = expression (a = 400, b = 20, c)
+            = expression ([a, b] = [10, 20]) = expression ([ a, b ] = [ 10, 20
+            ]) = expression(a, b, ...r) = expression // How to Declare
+            variables: let x = 5; let y = 6; // How to Compute values: let z = x
+            + y; // I am a Comment. I do Nothing
+        </script>
+    </body>
+</html>;
