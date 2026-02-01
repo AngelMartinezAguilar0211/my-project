80c0fa48749449b5bcfbd25bef9ee08920a444b7
Angel <angel.martinez@bunsan.io>
2026-02-01 16:26:16 -0600

    Security reenforcement

--- security.js
+++ security.js
@@ -1,9 +1,5 @@
 <html>
-    <head>
-        <title>Mi Primera Página JS</title>
-    </head>
     <body>
-        <h1>¡Hola desde HTML!</h1>
         <script>
             // Este es un comentario en JavaScript alert("¡Hola, mundo desde
             JavaScript!"); // Muestra una alerta console.log("¡Hola, mundo en la
